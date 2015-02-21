e86eaae635c1429b374c225e74ca28c152ac39da
ande4148 <ande4148@gmail.com>
2015-02-21 02:19:11 -0500

    homework solved

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The function makeCacheMatrix loads a designated matrix, calculates it's inverse, and
+## stores the inverse in the parent environment from which the matrix was called
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## The function cachesolve returns the inverse of an inverted matrix.  The inverted matrix is either
+## drawn from the parent environment if the original matrix has already been inverted, or, 
+## it inverts the original matrix so that it can have its inverse calculated and returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
